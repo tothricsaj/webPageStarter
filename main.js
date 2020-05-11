30c06d9996ad9142d0dd391120773331e9a8f7f3
tothricsaj <tothricsaj@gmail.com>
2020-05-11 21:09:16 +0200

    gitignore and mockFiles are added

--- main.js
+++ main.js
@@ -3,7 +3,12 @@ var fs = require('fs')
 function main() {
     let projectName = process.argv[2]
 
-    fs.mkdirSync(projectName)
+    if(projectName) {
+        fs.mkdirSync(projectName)
+    } else {
+        console.error('You must give a project name!')
+    }
+
 }
 
 main()
